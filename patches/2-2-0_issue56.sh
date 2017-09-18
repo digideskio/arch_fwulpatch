628a35302d1d51aa607dd3583cb47d802e018d94
steadfasterX <steadfastX@boun.cr>
2017-09-18 20:19:47 +0200

    new: fixing issue #56

---
+++ patches/2-2-0_issue56.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE: 2-2-0_issue56.sh
+# 
+#         USAGE: ./2-2-0_issue56.sh 
+# 
+#   DESCRIPTION: 
+# 
+#===============================================================================
+
+ME=$(id -u)
+
+[ $ME -ne 0 ] && echo "ERROR you need to run this script as root" && exit
+
+pacman -Q | grep downgrade || pacman --noconfirm -S downgrade
+
+adb kill-server
+yes | LANG=C downgrade android-tools-7.1.2_r27-1 --noconfirm
+find /home -type f -name adbkey* -exec rm {} \;
+
+
