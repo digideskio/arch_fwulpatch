37850493a618265648213c177d39c1f334f8a712
steadfasterX <steadfastX@boun.cr>
2017-09-19 15:48:19 +0200

    fix: missing logfile name

--- patches/2-2-0_issue56.sh
+++ patches/2-2-0_issue56.sh
@@ -5,7 +5,11 @@
 # 
 ########################################################################################
 
+# include and setup
 source /var/lib/fwul/generic.vars
+[ $? -ne 0 ] && echo "ERROR Cant include required lib" && exit
+BIN=${0##*/}
+LOG=$LOGPATH/${BIN/sh/log}
 
 ME=$(id -u)
 [ $ME -ne 0 ] && echo "ERROR you need to run this script as root" && exit
