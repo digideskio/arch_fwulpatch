0602e726e197ba6d38aab31ceeb6289d8c54fb95
steadfasterX <steadfastX@boun.cr>
2017-09-19 17:42:07 +0200

    new: increasing FWUL version number

--- patches/2-2-0_issue56.sh
+++ patches/2-2-0_issue56.sh
@@ -6,7 +6,7 @@
 ########################################################################################
 
 # include and setup
-source /var/lib/fwul/generic.vars
+source /var/lib/fwul/generic.vars 
 [ $? -ne 0 ] && echo "ERROR Cant include required lib" && exit
 BIN=${0##*/}
 LOG=$LOGPATH/${BIN/sh/log}
@@ -27,6 +27,9 @@ yes | LANG=C downgrade android-tools-7.1.2_r27-1 >> $LOG 2>&1
 echo 60
 
 find /home -type f -name adbkey* -exec rm -v {} \; >> $LOG 2>&1
+
+# increase version
+F_UPDVER "2.2.0"
 
 echo 100
 
