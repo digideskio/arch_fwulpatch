14b0dda9be9f4cb1f067090e2b0a1b4df04315ab
steadfasterX <steadfastX@boun.cr>
2017-09-20 12:11:24 +0200

    fix: increasing patchlevel & yad progress

--- patches/2-2-2_issue58.sh
+++ patches/2-2-2_issue58.sh
@@ -14,9 +14,18 @@ LOG=$LOGPATH/${BIN/sh/log}
 ME=$(id -u)
 [ $ME -ne 0 ] && echo "ERROR you need to run this script as root" && exit
 
+echo 20
 chown -v root.root /usr >> $LOG 2>&1
+echo 40
 chmod -v 755 /usr >> $LOG 2>&1
+echo 50
 chown -v root.root /usr/share >> $LOG 2>&1
+echo 60
 chmod -v 755 /usr/share >> $LOG 2>&1
+echo 80
+
+# increase version
+F_UPDVER "2.2" "2"
+echo 100
 
 F_LOG "patch applied"
